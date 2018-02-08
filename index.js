e95d6b062ec174bdfe0a51f5921d4dfdd3e10fb5
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-02-08 17:21:07 +0200

    Logging

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('critical', (resp) => {
+        console.log(resp);
         let dat = JSON.stringify(resp);
         socket.emit('changestate', dat);
 
